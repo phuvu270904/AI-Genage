67f932b88142657287e28679029cfe81d19d2375
phuvu <phu270904@gmail.com>
2024-09-22 20:37:38 +0700

    Auth UI base

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { logo } from './assets'
 import { Home, CreatePost } from './pages';
+import { Login, Register } from './pages/auth';
 
 
 const App = () => {
@@ -12,14 +13,23 @@ const App = () => {
           <img src={logo} alt='logo' className='w-28 object-contain' />
         </Link>
 
-        <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
-          Create
-        </Link>
+        <div>
+          <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+            Create
+          </Link>
+
+          <Link to="/login" className='ml-4'>
+            Login
+          </Link>
+        </div>
+        
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </main>
     </BrowserRouter>
